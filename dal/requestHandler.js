36f11f68c455042bf89b9ff1ad8f50a6b982a123
AntonGordei <49307594+AntonGordei@users.noreply.github.com>
2020-10-19 09:17:36 +0300

    use mssql

--- dal/requestHandler.js
+++ dal/requestHandler.js
@@ -1,36 +1,17 @@
-const Language = require('../dao/language')
-const Book = require('../dao/book')
-const Localization = require('../dao/localization')
+const sql = require('mssql')
 
-const getLanguageId = async (lang) => {
-    return await Language.findOne({where: {Code: lang}})
-        .then(res => res.LanguageId)
-        .catch(err=>console.log(err))
-}
-
-const getBooks = async () => {
-    return await Book.findAll({raw:true})
-        .then(books => books)
-        .catch(err=>console.log(err))
-}
-
-const getLocalisation = async (bookId, languageId) => {
-    return await Localization.findOne({where: {BookId: bookId, LanguageId: languageId}})
-        .then(res => res.Value)
-        .catch(err=>console.log(err))
-}
 
 const getBooksByLanguage = async (lang) => {
-
-    const booksList = []
-    const lngId = await getLanguageId(lang)
-    const books = await getBooks()
-    for (let i in books) {
-        const localisationName = await getLocalisation(books[i].BookId, lngId)
-        booksList.push({ Id: books[i].BookId, name: books[i].Name, ...(localisationName?{localisationName}: null) })
-    }
+    await sql.connect('mssql://test:test1234@localhost/localization')
+    const booksList = await sql.query`
+    SELECT b.BookId, ISNULL(i.Value,b.Name) as Name
+FROM Books b
+left join Localizations i
+on i.BookId=b.BookId AND LanguageId=(SELECT LanguageId FROM Languages WHERE Code = ${lang})
+    `
+    console.log(lang, booksList.recordset)
     return new Promise(((resolve) => {
-        resolve(JSON.stringify(booksList))
+        resolve(JSON.stringify(booksList.recordset))
     }))
 }
 
